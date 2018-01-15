44974e47eda1ebdeafafc3f1e7ab6a36e89f4026
ohanhi <ossi.hanhinen@gmail.com>
2018-01-15 21:05:28 +0200

    Add comments and remove extra function

--- completions/elm-package.fish
+++ completions/elm-package.fish
@@ -1,3 +1,5 @@
+# From the npm completions that shipped with fish
+
 function __fish_elm_package_needs_command
     set cmd (commandline -opc)
 
@@ -20,23 +22,19 @@ function __fish_elm_package_using_command
     return 1
 end
 
-function __fish_elm_package_needs_option
-    switch (commandline -ct)
-        case "-*"
-            return 0
-    end
-    return 1
-end
-
+# Install
 complete -f -c elm-package -n '__fish_elm_package_needs_command' -a 'install' -d 'Install packages to use locally'
 complete -f -c elm-package -n "__fish_elm_package_needs_command install" -s y -l yes -d "Reply 'yes' to all automated prompts."
 complete -f -c elm-package -n "__fish_elm_package_needs_command install" -s h -l help -d "Show this help text"
 
+# Publish
 complete -f -c elm-package -n '__fish_elm_package_needs_command' -a 'publish' -d 'Publish your package to the central catalog'
 complete -f -c elm-package -n "__fish_elm_package_needs_command publish" -s h -l help -d "Show this help text"
 
+# Bump
 complete -f -c elm-package -n '__fish_elm_package_needs_command' -a 'bump' -d 'Bump version numbers based on API changes'
 complete -f -c elm-package -n "__fish_elm_package_needs_command bump" -s h -l help -d "Show this help text"
 
+# Diff
 complete -f -c elm-package -n '__fish_elm_package_needs_command' -a 'diff' -d 'Get differences between two APIs'
 complete -f -c elm-package -n "__fish_elm_package_needs_command diff" -s h -l help -d "Show this help text"
