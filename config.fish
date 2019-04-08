2a440fe7eaf224c478277c11345cef380e8bfb12
ohanhi <ossi.hanhinen@gmail.com>
2019-04-08 09:42:42 +0300

    Add some local binary dirs

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 source ~/.aliases
 
-set PATH $PATH ./node_modules/.bin $HOME/.npm-global/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+set PATH $PATH ./node_modules/.bin $HOME/bin_own $HOME/.local/bin $HOME/.npm-global/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 
 set fish_color_cwd white
+
